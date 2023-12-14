9427ca7f8c318be9942d13dcee990586574a1aab
AHMEDNABIB <nabibpallab@gmail.com>
2023-12-14 20:37:23 +0600

    popular menu added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 
 
@@ -7,6 +8,7 @@ const Home = () => {
     return <div>
         <Banner></Banner>
         <Category></Category>
+        <PopularMenu></PopularMenu>
     </div>;
 };
 
