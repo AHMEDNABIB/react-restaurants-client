3d8c7ae42184bc041fa7aafdd72dc5a4a8ec108e
AHMEDNABIB <nabibpallab@gmail.com>
2023-12-19 01:12:58 +0600

    cart added to database complete

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
 import { useContext } from "react";
+
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { FaShoppingCart } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
 
+const NavBar = () => {
+	const { user, logOut } = useContext(AuthContext);
 
-const Navbar = () => {
-	  const { user, logOut } = useContext(AuthContext);
+	const[cart] = useCart()
 
-		const handleLogOut = () => {
-			logOut()
-				.then(() => {})
-				.catch((error) => console.log(error));
-		};
-    const navOptions = (
+	const handleLogOut = () => {
+		logOut()
+			.then(() => {})
+			.catch((error) => console.log(error));
+	};
+
+	const navOptions = (
 		<>
 			<li>
 				<Link to="/">Home</Link>
 			</li>
 			<li>
-				<Link to="/menu">Menu</Link>
+				<Link to="/menu">Our Menu</Link>
 			</li>
 			<li>
 				<Link to="/order/salad">Order Food</Link>
@@ -25,9 +30,18 @@ const Navbar = () => {
 			<li>
 				<Link to="/secret">Secret</Link>
 			</li>
+			<li>
+				<Link to="">
+					<button className="btn gap-2">
+						<FaShoppingCart></FaShoppingCart>
+						<div className="badge badge-secondary">
+							+{cart?.length || 0}
+						</div>
+					</button>
+				</Link>
+			</li>
 			{user ? (
 				<>
-					<span>{user?.displayName}</span>
 					<button onClick={handleLogOut} className="btn btn-ghost">
 						LogOut
 					</button>
@@ -41,15 +55,13 @@ const Navbar = () => {
 			)}
 		</>
 	);
-    return (
-		<div>
-			<div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl">
+
+	return (
+		<>
+			<div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
 				<div className="navbar-start">
 					<div className="dropdown">
-						<div
-							tabIndex={0}
-							role="button"
-							className="btn btn-ghost lg:hidden">
+						<label tabIndex={0} className="btn btn-ghost lg:hidden">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								className="h-5 w-5"
@@ -63,27 +75,26 @@ const Navbar = () => {
 									d="M4 6h16M4 12h8m-8 6h16"
 								/>
 							</svg>
-						</div>
+						</label>
 						<ul
 							tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {navOptions}
-							
+							className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+							{navOptions}
 						</ul>
 					</div>
-					<a className="btn btn-ghost text-xl">daisyUI</a>
+					<a className="btn btn-ghost normal-case text-xl">
+						Bistro Boss
+					</a>
 				</div>
 				<div className="navbar-center hidden lg:flex">
-					<ul className="menu menu-horizontal px-1">
-					{navOptions}
-					</ul>
+					<ul className="menu menu-horizontal px-1">{navOptions}</ul>
 				</div>
 				<div className="navbar-end">
-					<a className="btn">Button</a>
+					<a className="btn">Get started</a>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
-export default Navbar;
+export default NavBar;
